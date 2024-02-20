e0c392c4a0c8706a1dceae35c3ee6b4a2a2b187a
Jan Awadallah <jan.awadallah@gmail.com>
2024-02-21 01:28:56 +1100

    edit path

--- app/page.jsx
+++ app/page.jsx
@@ -43,23 +43,34 @@ const Dashboard = async () => {
   return (
     <div className="p-5">
       <div>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-        <h1>role: {session?.user.role}</h1>
         {tickets && tickets.length > 0 ? (
           uniqueCategories?.map((uniqueCategory, categoryIndex) => (
             <div key={categoryIndex} className="mb-4">
               <h2>{uniqueCategory}</h2>
               <div className="lg:grid grid-cols-2 xl:grid-cols-4 ">
-                {role === "admin" &&
-                  tickets
-                    .filter((ticket) => ticket.category === uniqueCategory)
-                    .map((filteredTicket, _index) => (
-                      <TicketCard
-                        id={_index}
-                        key={_index}
-                        ticket={filteredTicket}
-                      />
-                    ))}
+                {role === "admin"
+                  ? tickets
+                      .filter((ticket) => ticket.category === uniqueCategory)
+                      .map((filteredTicket, _index) => (
+                        <TicketCard
+                          id={_index}
+                          key={_index}
+                          ticket={filteredTicket}
+                        />
+                      ))
+                  : tickets
+                      .filter(
+                        (ticket) =>
+                          ticket.category === uniqueCategory &&
+                          ticket.userEmail === email
+                      )
+                      .map((filteredTicket, _index) => (
+                        <TicketCard
+                          id={_index}
+                          key={_index}
+                          ticket={filteredTicket}
+                        />
+                      ))}
               </div>
             </div>
           ))
