0b52f34fd686920b492192b474cf64bbf4f1dc93
Jan Awadallah <jan.awadallah@gmail.com>
2024-02-20 23:46:07 +1100

    edit path

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -2,9 +2,12 @@ import EditTicketForm from "@/app/(components)/EditTicketForm";
 
 const getTicketById = async (id) => {
   try {
-    const res = await fetch(`/api/Tickets/${id}`, {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `https://support.edgewatermc.com.au/api/Tickets/${id}`,
+      {
+        cache: "no-store",
+      }
+    );
 
     if (!res.ok) {
       throw new Error("Failed to fetch topic");
