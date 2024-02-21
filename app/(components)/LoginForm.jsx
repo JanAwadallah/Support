1268419e61b79f49db2b74b60ef1acd025563444
Jan Awadallah <jan.awadallah@gmail.com>
2024-02-21 18:28:58 +1100

    edit path

--- app/(components)/LoginForm.jsx
+++ app/(components)/LoginForm.jsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { signIn, useSession } from "next-auth/react";
+import { CirclesWithBar } from "react-loader-spinner";
 
 function LoginForm() {
   const router = useRouter();
   const { data: session } = useSession();
+  const [isLoading, setIsLoading] = useState(false);
   if (session?.user) {
     router.replace("/");
   }
@@ -25,6 +27,7 @@ function LoginForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
       const res = await signIn("credentials", {
         email: email,
@@ -35,6 +38,7 @@ function LoginForm() {
       if (res.ok) {
         setAuthorised(true);
         router.replace("/");
+        setIsLoading(false);
       } else if (res.error) {
         setError(res.error);
         return;
@@ -56,41 +60,58 @@ function LoginForm() {
         }}
         className="shadow-lg p-5 rounded-lg border-t-4 border-green-400 "
       >
-        <h1 className="text-xl font-bold my-4">Login</h1>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-3 items-center"
-        >
-          <input
-            pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-            title="Please enter a valid email address"
-            required
-            type="text"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
+        {isLoading ? (
+          <CirclesWithBar
+            height="100"
+            width="100"
+            color="#4fa94d"
+            outerCircleColor="#4fa94d"
+            innerCircleColor="#4fa94d"
+            barColor="#4fa94d"
+            ariaLabel="circles-with-bar-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
           />
-          <input
-            required
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="bg-green-600 text-white font-bold rounded-sm cursor-pointer px-6 py-2"
-          >
-            Login
-          </button>
-          {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-              {error}
-            </div>
-          )}
-          <Link className="text-sm mt-3 text-right" href={"/register"}>
-            <p>Do not have an account?</p>{" "}
-            <span className=" underline">Register</span>
-          </Link>
-        </form>
+        ) : (
+          <>
+            <h1 className="text-xl font-bold my-4">Login</h1>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-3 items-center"
+            >
+              <input
+                pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+                title="Please enter a valid email address"
+                required
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                required
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="submit"
+                className="bg-green-600 text-white font-bold rounded-sm cursor-pointer px-6 py-2"
+              >
+                Login
+              </button>
+              {error && (
+                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                  {error}
+                </div>
+              )}
+              <Link className="text-sm mt-3 text-right" href={"/register"}>
+                <p>Do not have an account?</p>{" "}
+                <span className=" underline">Register</span>
+              </Link>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
